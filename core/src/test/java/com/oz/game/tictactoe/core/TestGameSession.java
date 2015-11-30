0fe6a3cc872e2c8ec1940e524944781fac5382b0
Ozgur <ozguramac@users.noreply.github.com>
2015-11-29 23:00:40 -0500

    Added test for game session: Increased code coverage.

--- core/src/test/java/com/oz/game/tictactoe/core/TestGameSession.java
+++ core/src/test/java/com/oz/game/tictactoe/core/TestGameSession.java
@@ -1,9 +1,62 @@
 package com.oz.game.tictactoe.core;
 
+import com.oz.game.tictactoe.core.io.GameInput;
+import com.oz.game.tictactoe.core.io.GameOutput;
+import com.oz.game.tictactoe.core.persist.PersistEntry;
+import com.oz.game.tictactoe.core.persist.PersistFacade;
+import com.oz.game.tictactoe.core.persist.PersistenceException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by developer on 11/12/15.
  */
 
+@RunWith(org.mockito.runners.MockitoJUnitRunner.class)
 public class TestGameSession {
-    //TODO: Cover game session, config, negative history with persistence exception
+    @Mock private GameInput mockIn;
+    @Mock private GameOutput mockOut;
+    @Mock private PersistFacade mockFacade;
+    private GameSession gameSession;
+
+    @Before
+    public void beforeTest() throws Exception {
+        gameSession = new GameSession(
+                new GameConfig()
+                .playerOne(GameConfig.PlayerType.COMPUTER)
+                .playerTwo(GameConfig.PlayerType.COMPUTER)
+                .persistFacade(mockFacade)
+                .input(mockIn)
+                .output(mockOut)
+                .printState(true)
+        );
+    }
+
+    @Test
+    public void testPlay() throws Exception {
+        do {
+            gameSession.play();
+        } while (false == gameSession.isGameOver());
+
+        gameSession.logStats();
+    }
+
+    @Test
+    public void testError() throws Exception {
+        final PersistenceException pe = new PersistenceException("Testing!!");
+        when(mockFacade.matchBest(any(PersistEntry.class))).thenThrow(pe);
+
+        gameSession.play();
+
+        verify(mockFacade).matchBest(any(PersistEntry.class));
+    }
 }
